caca9cde4466be1202f03bd0672081bb916acb27
Gabriel Cavalcante <gscavalcante@gmail.com>
2019-03-04 15:03:37 -0300

    Add question of the exercise 2

---
+++ src/com/gcavalcante/garg/unit1/exercise2/Exercise2.java
@@ -0,0 +1,20 @@
+package com.gcavalcante.garg.unit1.exercise2;
+
+/*
+ * Write a Java class that allows parallel search in an array of integer. It
+ * provides the following `static` method:
+ * 
+ * `public static int parallelSearch(int x, int[] A , int numThreads)`
+ * 
+ * This method creates as many threads as specified by numThreads, divides the
+ * array A into that many parts, and gives each thread a part of the array to
+ * search for sequentially. If any thread finds x, then it returns an
+ * index i such that A [i] = x. Otherwise, the method returns -1.
+ */
+public class Exercise2 {
+
+	public static int parallelSearch(int x, int[] A, int numThreads) {
+		return -1;
+	}
+
+}
