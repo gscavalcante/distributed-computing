a58a4a14178736139ed470ffb6a65f0fee32f06a
Gabriel Cavalcante <gscavalcante@gmail.com>
2019-03-06 21:47:21 -0300

    Add the question of exercise 4 from the 1st unit

---
+++ src/com/gcavalcante/garg/unit1/exercise4/Exercise4.java
@@ -0,0 +1,10 @@
+package com.gcavalcante.garg.unit1.exercise4;
+
+/*
+ * Write a multithreaded program in Java that sorts an array using recursive
+ * merge sort. The main thread forks two threads to sort the two halves of
+ * arrays, which are then merged.
+ */
+public class Exercise4 {
+
+}
